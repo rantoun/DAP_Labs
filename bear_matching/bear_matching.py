cfe8fb03596a74f803c6c25c004a7ae643f5b0da
Rami Antoun <reantoun@gmail.com>
2014-07-25 11:31:46 -0500

    bear_matching update

--- bear_matching/bear_matching.py
+++ bear_matching/bear_matching.py
@@ -14,6 +14,7 @@ def matching_bears(filename):
 	fam_dict = {}
 	male_bears = []
 	female_bears = []
+	parents = []
 
 	for line in file_text:
 		line = line.rstrip()
@@ -35,11 +36,6 @@ def matching_bears(filename):
 	for bear in bears_dict:
 		mother = parents_dict[bear][0]
 		father = parents_dict[bear][1]
-		
-		if bears_dict[bear][0] == 'm':
-			male_bears.append(bear)
-		elif bears_dict[bear][0] == 'f':
-			female_bears.append(bear)
 
 		try:
 			grandparents_dict[bear] = [parents_dict[mother][0], parents_dict[mother][1], parents_dict[father][0], parents_dict[father][1]]
@@ -53,27 +49,30 @@ def matching_bears(filename):
 
 		fam_dict[bear] = parents_dict[bear] + grandparents_dict[bear]
 
-	gender_combinations = list(product(female_bears, male_bears))
-
-	for pair in gender_combinations[:]:
-		if abs(bears_dict[pair[0]][3] - bears_dict[pair[1]][3]) > 10 or (bears_dict[pair[0]][3] > 60 or bears_dict[pair[1]][3] > 60) or (bears_dict[pair[0]][3] < 20 or bears_dict[pair[1]][3] < 20):
-			gender_combinations.remove(pair)
-
 	for bear in bears_dict:
-		for pair in gender_combinations[:]:
-			if pair[0] in bears_dict[bear] or pair[1] in bears_dict[bear]:
-				gender_combinations.remove(pair)
+		if (bears_dict[bear][3] > 60 or bears_dict[bear][3] < 20):
+			continue
+		elif bears_dict[bear][0] == 'm':
+			male_bears.append(bear)
+		elif bears_dict[bear][0] == 'f':
+			female_bears.append(bear)
 
-	for pair in gender_combinations[:]:
-		if parents_dict[pair[0]] != ['nil','nil'] or parents_dict[pair[1]] != ['nil','nil']:
-			if (parents_dict[pair[0]][1] == parents_dict[pair[1]][1]) or parents_dict[pair[0]][1] == parents_dict[pair[1]][1]:
-				gender_combinations.remove(pair)
+	pairs = list(product(female_bears, male_bears))
+
+	[parents.append(parent[0]) for parent in parents_dict.values()]
+	[parents.append(parent[1]) for parent in parents_dict.values()]
 
 	for i in range(6):
-		for pair in gender_combinations[:]:
-			if len(fam_dict[pair[0]]) >= 4 and len(fam_dict[pair[1]]) >= 4:
+		for pair in pairs[:]:
+			if pair[0] in parents or pair[1] in parents:
+				pairs.remove(pair)
+			elif abs(bears_dict[pair[0]][3] - bears_dict[pair[1]][3]) > 10:
+				pairs.remove(pair)
+			elif len(fam_dict[pair[0]]) >= 4 and len(fam_dict[pair[1]]) >= 4:
 				if fam_dict[pair[0]][i] in fam_dict[pair[1]]:
-					gender_combinations.remove(pair)
-						
-	return gender_combinations
-    
+					pairs.remove(pair)
+
+	return pairs
+
+
+
